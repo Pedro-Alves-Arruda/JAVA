82e3a5bbad508d0ffc5ce6c519f2f89328c2d4c3
Pedro-Alves-Arruda <pedro.alves.arruda10@gmail.com>
2022-01-09 21:51:30 -0200

    Apaguei os comentarios da classe main deixando ela mais clean, apenas com a criação do meu objeto da classe apresentar e já chamando o metodo construtor que chama os outros metodos para dar continuidade no programa.

--- Animais/src/main.java
+++ Animais/src/main.java
@@ -4,36 +4,8 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//gato gatos[] = new gato[4];
 		apresentar apresenta = new apresentar();
 	
-		/*gatos[1] = new gato();
-		gatos[1].setCorOlho("Azul");
-		gatos[1].setSexo("M");
-		
-		System.out.println(gatos[1].getCorOlho());
-		System.out.println(gatos[1].getSexo());
-		*/
-		/*for(int cont = 0; cont < gatos.length; cont++) {
-			
-			Scanner entrada = new Scanner(System.in);
-			
-			gatos[cont] = new gato();
-			
-			System.out.println("Digite a cor do olho do seu gatinho:");
-			gatos[cont].setCorOlho(entrada.next());
-			
-			System.out.println("Digite o sexo agora:");
-			gatos[cont].setSexo(entrada.next());
-		}
-		System.out.println("Você tem" + gatos.length+" gatinhos");
-		
-		for(int cont = 0; cont < gatos.length; cont++) {
-		
-			System.out.println("O " + (cont+1) + " gatinho tem olho " + gatos[cont].getCorOlho() + " e é do sexo " + gatos[cont].getSexo());
-		}
-		*/
 	}
 	
 }
